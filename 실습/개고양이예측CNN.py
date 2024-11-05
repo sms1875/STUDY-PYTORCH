2c964d0f4e46e24ca82bf0727c2f1fa1431acf47
송명석 <67058185+sms1875@users.noreply.github.com>
2024-11-05 15:18:27 +0900

    개고양이

--- 실습/개고양이예측CNN.py
+++ 실습/개고양이예측CNN.py
@@ -1,16 +1,15 @@
+import torch
 import torch.nn as nn
 import torch.optim as optim
-import torch
 import torchvision
 import torchvision.transforms as transforms
 from torch.utils.data import DataLoader
 import os
-import kagglehub
 
 # CNN 모델 정의
-class SimpleCNN(nn.Module):
+class CNN(nn.Module):
     def __init__(self):
-        super(SimpleCNN, self).__init__()
+        super(CNN, self).__init__()
         self.conv1 = nn.Conv2d(3, 32, kernel_size=3, padding=1)
         self.conv2 = nn.Conv2d(32, 64, kernel_size=3, padding=1)
         self.conv3 = nn.Conv2d(64, 128, kernel_size=3, padding=1)
@@ -29,14 +28,10 @@ class SimpleCNN(nn.Module):
         x = self.dropout(x)
         x = self.fc2(x)
         return x
-    
-# Kaggle 데이터셋 다운로드
-path = kagglehub.dataset_download("tongpython/cat-and-dog")
-print("Path to dataset files:", path)
 
-# 데이터 경로 설정
-train_dir = os.path.join(path, "training_set/training_set")
-test_dir = os.path.join(path, "test_set/test_set")
+# 데이터 경로 설정 (로컬 경로)
+train_dir = "/workspaces/STUDY-PYTORCH/kaggle/Cat and Dog/training_set/training_set"
+test_dir = "/workspaces/STUDY-PYTORCH/kaggle/Cat and Dog/test_set/test_set"
 
 # 데이터 전처리 정의
 transform = transforms.Compose([
@@ -50,12 +45,12 @@ train_dataset = torchvision.datasets.ImageFolder(root=train_dir, transform=trans
 test_dataset = torchvision.datasets.ImageFolder(root=test_dir, transform=transform)
 
 # DataLoader 설정
-train_loader = DataLoader(train_dataset, batch_size=64, shuffle=True, num_workers=4)
-test_loader = DataLoader(test_dataset, batch_size=64, shuffle=False, num_workers=4)
+train_loader = DataLoader(train_dataset, batch_size=64, shuffle=True, num_workers=0)
+test_loader = DataLoader(test_dataset, batch_size=64, shuffle=False, num_workers=0)
 
 # 모델 초기화 및 설정
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
-model = SimpleCNN().to(device)
+model = CNN().to(device)
 criterion = nn.CrossEntropyLoss()
 optimizer = optim.Adam(model.parameters(), lr=0.001)
 
@@ -81,6 +76,16 @@ for epoch in range(epochs):
     print(f"Epoch {epoch+1}/{epochs}, Loss: {running_loss/len(train_loader)}")
 
 print("Finished Training")
+# Epoch 1/10, Loss: 0.6654650682494754
+# Epoch 2/10, Loss: 0.5780091552980362
+# Epoch 3/10, Loss: 0.5247365792119314
+# Epoch 4/10, Loss: 0.46941256523132324
+# Epoch 5/10, Loss: 0.4167997170062292
+# Epoch 6/10, Loss: 0.352424107787628
+# Epoch 7/10, Loss: 0.2809536666387603
+# Epoch 8/10, Loss: 0.20615432639088896
+# Epoch 9/10, Loss: 0.15371812350811467
+# Epoch 10/10, Loss: 0.10207413670621694
 
 # 모델 평가
 model.eval()
@@ -100,3 +105,4 @@ with torch.no_grad():
 
 accuracy = 100 * correct / total
 print(f"Accuracy of the model on the test images: {accuracy:.2f}%")
+# Accuracy of the model on the test images: 78.30%
